35762ed06bf5e8d9fbb5d7749dcb119743f4480c
Arthur Senna de Andrade Souza <44761640804@info.local>
2019-05-13 14:04:12 -0300

    CaixaEletronico

---
+++ CaixaEletronicoCompleto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CaixaEletronicoCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] notas = {100, 50, 20, 10, 5, 2, 1};
+
+            Console.Write("Digite o valor do seu saque: ");
+            int saque = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < notas.Length; i++)
+            {
+                int qntdeNotas = saque / notas[i];
+                saque = saque%notas[i];
+                if(qntdeNotas !=0)
+                {
+                    Console.WriteLine($"Você recebeu {qntdeNotas} nota(s) de {notas[i]}");
+                }
+            }
+        }
+    }
+}
